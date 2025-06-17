12738c7409d4ae9ee5391cb5887f339ab38a42f1
Yesayi Babakhanyan <63794677+yesayi98@users.noreply.github.com>
2025-06-17 13:38:42 +0400

    Update Parser.php

--- lib/CrEOF/Geo/String/Parser.php
+++ lib/CrEOF/Geo/String/Parser.php
@@ -482,7 +482,7 @@ class Parser
     {
         $expected = sprintf('Expected %s, got', $expected);
         $token    = $this->lexer->lookahead;
-        $found    = null === $this->lexer->lookahead ? 'end of string.' : sprintf('"%s"', $token['value']);
+        $found    = null === $this->lexer->lookahead ? 'end of string.' : sprintf('"%s"', $token->value);
 
         $message = sprintf(
             '[Syntax Error] line 0, col %d: Error: %s %s in value "%s"',
